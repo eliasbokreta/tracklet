325962e2ee89c81e5dc9f4056ea46889bdb38a03
eliasbokreta <elias.bokreta@gmail.com>
2022-04-25 08:30:31 +0200

    handling parameters and hmac signature

--- pkg/binance/client.go
+++ pkg/binance/client.go
@@ -1,88 +1,109 @@
 package binance
 
 import (
+	"crypto/hmac"
+	"crypto/sha256"
+	"encoding/hex"
 	"fmt"
 	"io/ioutil"
 	"net/http"
 	"net/url"
 	"os"
-	"strings"
 	"time"
 )
 
 type Client struct {
-	HTTPClient      *http.Client
-	BaseUrl         string
-	APIKey          string
-	SecretKey       string
-	RetryDelay      time.Duration
-	MaxRetries      int
-	TotalDaysOfData time.Duration
+	HTTPClient *http.Client
+	BaseUrl    string
+	APIKey     string
+	SecretKey  string
+	RetryDelay time.Duration
+	MaxRetries int
 }
 
-func NewClient(baseUrl string, apiKey string, secretKey string, timeout time.Duration, retryDelay time.Duration, maxRetries int, totalDaysOfData time.Duration) *Client {
+func NewClient(baseUrl string, apiKey string, secretKey string, timeout time.Duration, retryDelay time.Duration, maxRetries int) *Client {
 	return &Client{
 		HTTPClient: &http.Client{
 			Timeout: time.Second * timeout,
 		},
-		BaseUrl:         baseUrl,
-		APIKey:          apiKey,
-		SecretKey:       secretKey,
-		RetryDelay:      retryDelay,
-		MaxRetries:      maxRetries,
-		TotalDaysOfData: totalDaysOfData,
+		BaseUrl:    baseUrl,
+		APIKey:     apiKey,
+		SecretKey:  secretKey,
+		RetryDelay: retryDelay,
+		MaxRetries: maxRetries,
 	}
 }
 
-func buildQueryString(params map[string]string) strings.Reader {
-	values := url.Values{}
+func (c *Client) generateSignature(queryString string) (string, error) {
+	mac := hmac.New(sha256.New, []byte(c.SecretKey))
+	_, err := mac.Write([]byte(queryString))
+	digest := mac.Sum(nil)
+	if err != nil {
+		return "", fmt.Errorf("could not generate hmac")
+	}
+	hexDigest := hex.EncodeToString(digest)
+	return hexDigest, nil
+}
+
+func (c *Client) buildQueryString(q url.Values, params map[string]string) url.Values {
+	if len(params) == 0 {
+		return nil
+	}
 	for elem := range params {
-		values.Add(elem, params[elem])
+		q.Add(elem, params[elem])
 	}
-	query := values.Encode()
 
-	return *strings.NewReader(query)
+	signature, err := c.generateSignature(q.Encode())
+	if err != nil {
+		return nil
+	}
+
+	q.Add("signature", signature)
+
+	return q
 }
 
-func (c *Client) request(endpoint string) ([]byte, error) {
-	parameters := buildQueryString(map[string]string{})
-
-	req, err := http.NewRequest("GET", fmt.Sprintf("%s%s", c.BaseUrl, endpoint), &parameters)
+func (c *Client) request(endpoint string, parameters map[string]string) ([]byte, error) {
+	req, err := http.NewRequest("GET", fmt.Sprintf("%s%s", c.BaseUrl, endpoint), nil)
 	if err != nil {
 		return nil, err
+	}
+
+	q := req.URL.Query()
+	queryString := c.buildQueryString(q, parameters)
+	if queryString != nil {
+		req.URL.RawQuery = queryString.Encode()
 	}
 
 	req.Header.Add("X-MBX-APIKEY", c.APIKey)
 
 	response, err := c.HTTPClient.Do(req)
 	if err != nil || response.StatusCode != 200 {
-		return nil, fmt.Errorf("error while executing request")
+		return nil, fmt.Errorf("error while executing request: %+v", response.Status)
 	}
 	defer response.Body.Close()
 
 	body, err := ioutil.ReadAll(response.Body)
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("error while reading body")
 	}
 
 	return body, nil
 }
 
-func (c *Client) RequestWithRetries(endpoint string) ([]byte, error) {
+func (c *Client) RequestWithRetries(endpoint string, parameters map[string]string) ([]byte, error) {
 	var body []byte
 	var err error
 	retries := 0
 
 	for {
-		body, err = c.request(endpoint)
+		body, err = c.request(endpoint, parameters)
 		if err == nil || retries == c.MaxRetries {
 			break
 		}
 
 		retries += 1
-		fmt.Fprintf(os.Stderr, "Request error: %+v\n", err)
-		fmt.Fprintf(os.Stderr, "Retrying in: %+v\n", c.RetryDelay)
-		fmt.Fprintf(os.Stderr, "Retry : %+v/%+v\n", retries, c.MaxRetries)
+		fmt.Fprintf(os.Stderr, "%+v (Retry %+v/%+v)\n", err, retries, c.MaxRetries)
 		time.Sleep(time.Second * c.RetryDelay)
 	}
 
