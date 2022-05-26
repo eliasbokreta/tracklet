58e5ddfdba1b17aa498d5172ebe88c8cbb0e9f72
eliasbokreta <elias.bokreta@gmail.com>
2022-05-26 20:04:04 +0200

    added default values for configuration

--- pkg/utils/config.go
+++ pkg/utils/config.go
@@ -2,40 +2,64 @@ package utils
 
 import (
 	"fmt"
+	"time"
 
 	"github.com/spf13/viper"
 )
 
 type Config struct {
+	Tracklet  Tracklet            `mapstructure:"tracklet"`
 	Exchanges map[string]Exchange `mapstructure:"exchanges"`
 }
 
-type Exchange struct {
-	APIKey    string `mapstructure:"apiKey"`
-	SecretKey string `mapstructure:"secretKey"`
+type Tracklet struct {
+	MaxHistory int           `mapstructure:"maxHistory"`
+	Timeout    time.Duration `mapstructure:"timeout"`
+	RetryDelay time.Duration `mapstructure:"retryDelay"`
+	MaxRetries int           `mapstructure:"maxRetries"`
 }
 
+type Exchange struct {
+	APIKey       string   `mapstructure:"apiKey"`
+	SecretKey    string   `mapstructure:"secretKey"`
+	IncludePairs []string `mapstructure:"includePairs"`
+}
+
+// Create a new Config object
 func NewConfig() *Config {
 	return &Config{}
 }
 
-func (c *Config) LoadConfig(path string) error {
+// Set configuration default values
+func setDefaultValues() {
+	viper.SetDefault("tracklet.maxHistory", 365)
+	viper.SetDefault("tracklet.timeout", 30)
+	viper.SetDefault("tracklet.retryDelay", 5)
+	viper.SetDefault("tracklet.maxRetries", 10)
+
 	viper.SetDefault("exchanges.binance", Exchange{})
+	viper.SetDefault("exchanges.binance.apiBaseUrl", "https://api.binance.com")
 	viper.SetDefault("exchanges.coinbase", Exchange{})
 	viper.SetDefault("exchanges.kucoin", Exchange{})
+}
 
-	viper.AddConfigPath(path)
+// Load configuration file
+func (c *Config) LoadConfig() error {
+	viper.AddConfigPath("./config")
+	viper.AddConfigPath("$HOME/.tracklet")
 	viper.SetConfigName("tracklet")
 	viper.SetConfigType("yaml")
 
+	setDefaultValues()
+
 	err := viper.ReadInConfig()
 	if err != nil {
-		return fmt.Errorf("could not retrieve config file: %s", err.Error())
+		return fmt.Errorf("could not retrieve config file: %v", err)
 	}
 
 	err = viper.Unmarshal(&c)
 	if err != nil {
-		return fmt.Errorf("error unmarshalling config file: %s", err.Error())
+		return fmt.Errorf("error unmarshalling config file: %v", err)
 	}
 
 	return nil
